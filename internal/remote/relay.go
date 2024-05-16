dc5b87b93c4c4e7cb8324826c285be701824e37c
thebinary <binary4bytes@gmail.com>
2024-05-16 08:20:34 +0545

    send `RemoteMsg` rather than `KeyMsg` from remote relay

--- internal/remote/relay.go
+++ internal/remote/relay.go
@@ -1,6 +1,9 @@
 package remote
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/maaslalani/slides/internal/model"
+)
 
 // CommandRelay is meant to expose slide interaction to external
 // processes that can work as a remote for the slides.
@@ -15,14 +18,14 @@ func NewCommandRelay(p *tea.Program) *CommandRelay {
 }
 
 func (r *CommandRelay) SlideNext() {
-	r.Send(tea.KeyMsg{
+	r.Send(model.RemoteMsg{
 		Type:  tea.KeyRunes,
 		Runes: []rune{'n'},
 	})
 }
 
 func (r *CommandRelay) SlidePrev() {
-	r.Send(tea.KeyMsg{
+	r.Send(model.RemoteMsg{
 		Type:  tea.KeyRunes,
 		Runes: []rune{'p'},
 	})
@@ -31,38 +34,38 @@ func (r *CommandRelay) SlidePrev() {
 func (r *CommandRelay) SlideFirst() {
 	// Requires 2 keystrokes to actually
 	// move to first slide
-	r.Send(tea.KeyMsg{
+	r.Send(model.RemoteMsg{
 		Type:  tea.KeyRunes,
 		Runes: []rune{'g'},
 	})
-	r.Send(tea.KeyMsg{
+	r.Send(model.RemoteMsg{
 		Type:  tea.KeyRunes,
 		Runes: []rune{'g'},
 	})
 }
 
 func (r *CommandRelay) SlideLast() {
-	r.Send(tea.KeyMsg{
+	r.Send(model.RemoteMsg{
 		Type:  tea.KeyRunes,
 		Runes: []rune{'G'},
 	})
 }
 
 func (r *CommandRelay) CodeExecute() {
-	r.Send(tea.KeyMsg{
+	r.Send(model.RemoteMsg{
 		Type: tea.KeyCtrlE,
 	})
 }
 
 func (r *CommandRelay) CodeCopy() {
-	r.Send(tea.KeyMsg{
+	r.Send(model.RemoteMsg{
 		Type:  tea.KeyRunes,
 		Runes: []rune{'y'},
 	})
 }
 
 func (r *CommandRelay) Quit() {
-	r.Send(tea.KeyMsg{
+	r.Send(model.RemoteMsg{
 		Type:  tea.KeyRunes,
 		Runes: []rune{'q'},
 	})
